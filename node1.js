a718bba1d4116e185af2708cd5b4ba5af56df9f3
ABDO ELGENDI <anahinak32@gmail.com>
2019-02-26 16:18:20 +0200

    no change

---
+++ node1.js
@@ -0,0 +1,6 @@
+// var http=require('http');
+// http.createServer(function(req,res1){
+//  res1.writeHead(200,{'Content-Type':'text/html'});
+//  res1.end('with the name of allah');
+// }).listen(8000);
+ console.log("with the name of allah");
