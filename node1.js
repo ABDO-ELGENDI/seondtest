f6ee9d6b0dc77355371b9eb190f55100c0a67764
ABDO ELGENDI <anahinak32@gmail.com>
2019-03-02 12:49:22 +0200

    removed

--- node1.js
+++
@@ -1,9 +0,0 @@
-// var http=require('http');
-// http.createServer(function(req,res1){
-//  res1.writeHead(200,{'Content-Type':'text/html'});
-//  res1.end('with the name of allah');
-// }).listen(8000);
- console.log("with the name of allah");
-
- console.log("hi");
- console.log("hi,too");
